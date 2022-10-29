d2bb5f8efb2780c5b9957ae6ee4dba8372bd193b
alexey <alexnagornnyy.an@gmail.com>
2022-10-29 22:28:45 +0100

    moved fuctionality to server file

--- oracle-bot/src/index.ts
+++ oracle-bot/src/index.ts
@@ -1,18 +1,30 @@
-import TelegramService from "./services/telegramService";
-import { sleep } from "./utils/utils";
+import {sleep} from "./utils/utils";
+import {Wallet} from "fuels";
+import {SEED} from "./config";
+import {OracleAbi__factory} from "./contracts";
+import axios from "axios";
+import BN from "./utils/BN";
 
-const { log, groupMessage } = new TelegramService();
+// const { log, groupMessage } = new TelegramService();
 
-//For normal bot life, the account must have enough money to pay commissions
-//The bot will keep its funds in USDT, respectively, there should be enough of them on the account
+const CONTRACT_ID =
+    "0x7cb52c0c6f43fceaac13ade73d7ee8531dea944320fa5038c7133939fe3f079b";
+const wallet = new Wallet(
+    SEED,
+    "https://node-beta-1.fuel.network/graphql"
+);
+const contract = OracleAbi__factory.connect(CONTRACT_ID, wallet);
 
-const blackList = ["AbunLGErT5ctzVN8MVjb4Ad9YgjpubB8Hqb17VxzfAck"];
-
-// user says "im selling 1 PUZZLE for 18 USDN"
-// token0 = PUZZLE, token1 = USDN
-// amount0 = 1 * 10**8, amount1 = 18 * 10**6
 (async () => {
   while (true) {
+    //todo add another sources
+    const { data } = await axios.get(
+        "https://api.coingecko.com/api/v3/simple/price?ids=ethereum%2Cdai&vs_currencies=usd&include_market_cap=false&include_24hr_vol=false&include_24hr_change=false&include_last_updated_at=false&precision=18"
+    );
+    //todo add another coins
+    await contract.functions.set_price(
+        BN.parseUnits(data.ethereum.usd, 18).toString()
+    );
     await sleep(1000);
   }
 })();
